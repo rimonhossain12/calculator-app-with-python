9f1eb1bcd8ad9f895c18285afebe924af8f00038
rimonspi12 <rimonmpi@gmail.com>
2022-06-16 13:08:54 +0600

    square root button show ui

--- Calculator.py
+++ Calculator.py
@@ -47,6 +47,8 @@ class Calculator:
     def create_special_buttons(self):
         self.create_clear_button()
         self.create_equals_button()
+        self.create_square_button()
+        self.create_sqrt_button()
 
     def create_display_labels(self):
         total_label = tk.Label(self.display_frame,text=self.total_expression,anchor=tk.E,bg=LIGHT_GRAY,fg=LABEL_COLOR, padx=24,font=SMALL_FONT_STYLE)
@@ -93,10 +95,30 @@ class Calculator:
         self.update_label()
         self.update_total_label()
 
+
     def create_clear_button(self):
         button = tk.Button(self.buttons_frame, text="C", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                           borderwidth=0,command=self.clear())
-        button.grid(row=0, column=1, columnspan=3, sticky=tk.NSEW)
+                           borderwidth=0,command=self.clear)
+        button.grid(row=0, column=1,sticky=tk.NSEW)
+
+    def square(self):
+        self.current_expression = str(eval(f"{self.current_expression}**0.5"))
+        self.update_label()
+
+    def create_square_button(self):
+        button = tk.Button(self.buttons_frame, text="x\u00b2", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                           borderwidth=0,command=self.square)
+        button.grid(row=0, column=2,sticky=tk.NSEW)
+
+    def sqrt(self):
+            self.current_expression = str(eval(f"{self.current_expression}**0.5"))
+            self.update_label()
+
+    def create_sqrt_button(self):
+            button = tk.Button(self.buttons_frame, text="\u221ax", bg=OFF_WHITE, fg=LABEL_COLOR,
+                               font=DEFAULT_FONT_STYLE,
+                               borderwidth=0, command=self.sqrt)
+            button.grid(row=0, column=3, sticky=tk.NSEW)
 
 
     def evaluate(self):
